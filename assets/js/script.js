5ac0c70bccca48cddd9cfa3c7a25d2a14613c4e8
AndrewRobins51413 <h.andrew.robins@gmail.com>
2019-10-06 20:01:46 -0700

    not done, but pretty close.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,4 @@
 //memory-matching game
-// nine pairs of cards to be
-    // Shuffled
-    //assigned
-    // flip on click
-    // acknowledge match = winner
-
-// First: make 9 pairs; 1-9 of hearts  ,   of ${suits[suit]}`
 
 $(document).ready(initializeApp);
 console.log('initializeApp');
@@ -15,26 +8,25 @@ var secondCardClicked = null;
 var firstImageCompare = null;
 var secondImageCompare = null;
 var matches = null;
+var click = null;
 
-function initializeApp(){
-  $(".card").on("click", handleCardClick);
-}
+//var numMatches = $('<div>').text(add.matches)
+
+
+function initializeApp(){$(".card").on("click", handleCardClick);}
+
 // Three-five lines above - #card1 is where the click (event) happens|| ".on" is JQ$
 // function() is an anonymous function which calls the handleCardClick, but
 // is a callback b/c there are no () immediately after the f() name.
 
 function handleCardClick(event){
-// $(event.currentTarget);
-// flipCardBack
-  // console.log('Card image url clicked', $(event.currentTarget).class('background-image'));
-// $(event.currentTarget).flipCardBack.hide("div:first-child");
-
     if (firstCardClicked === null) {          //once first card is defined,
         firstCardClicked = $(event.currentTarget).find('.flipCardBack'); //defining non-null value to FCC
         console.log('First Card clicked', firstCardClicked)
         firstCardClicked.addClass('hidden');
       firstImageCompare = $(event.currentTarget).find('.flipCardFront').css("background-image");
       console.log("first image:", firstImageCompare);
+      click = click + 1;
     }
     else {
       //  (secondCardClicked === null && firstCardClicked === true){
@@ -43,42 +35,42 @@ function handleCardClick(event){
       console.log('Second Card Back:', secondCardClicked);
       secondImageCompare = $(event.currentTarget).find('.flipCardFront').css("background-image");
       console.log("second image:", secondImageCompare);
+      click = click+1;
     }
 
   if (secondImageCompare === firstImageCompare) {
-    alert("Winner!")
+    console.log("cards match");
+    resetStats()
+    setTimeout(function () {
+      firstCardClicked.removeClass('hidden');
+      secondCardClicked.removeClass('hidden')
+    }, 2500);
+    matches = matches + 1;
 
   } else if (secondImageCompare !== null ) {
-
-
-    setTimeout( function(){
+    console.log("No Match");
+    resetStats()
+    setTimeout(function () {            // setTimeout (function(){
       firstCardClicked.removeClass('hidden');
-      secondCardClicked.removeClass('hidden');
-    }, 1000);
+      secondCardClicked.removeClass('hidden')
+     }, 1500);                 // codeblock)}, milliseconds);
+    }
 
-   // setTimeout (function(){
-   // codeblock)}, milliseconds);
+  if (secondImageCompare !== null) {
+    var winPercent =  function winPercentCalc(){(matches/click)*100};
+  }
+console.log("testing matches", matches);
+console.log("testing clicks", click);
+console.log("percent win", winPercent);
 
-}
-
-
-
+  function resetStats() {       //this does not appear to be resetting the variables.
+    var firstCardClicked = null;    //this has scope written all over it.
+    var secondCardClicked = null;    //I'm trying to re-set global variables from inside
+    var firstImageCompare = null;   // a function.
+    var secondImageCompare = null;
 
   }
 
+}
 
-
-
-
-var deck = [2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-    //const suits = ['Hearts'];
-    const values = [ 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-console.log(deck);
-
-
-// For the indecisive and culinarily adventurous: Dinner Roulette! With one click a locally
-// prepared meal will be on its way to your doorstep, the twist is, you won't
-// know what is for dinner until you open the box. User can adjust dish
-//possibilities based on budget and dietary preferences. You click, we choose!
+// resetting the game
